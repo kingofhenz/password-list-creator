6c41b0be73274415777b2a13e98ddb2c7da9c32a
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-08-17 14:12:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import string
 def guess_password():
     chars = string.ascii_lowercase + string.digits
     for password_length in range(1, 9):
+        for guess in itertools.product(chars, repeat=password_length):
+            with open("password.txt", "a") as f:
+                guess = ''.join(guess)
+                f.write(guess)
+                f.write("\n")
